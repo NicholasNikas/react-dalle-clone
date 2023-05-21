4d8a7064314ae707b8d71f9a349e3558ccaa3e25
Nicholas Nikas <nnikas@microsoft.com>
2023-05-20 22:03:41 -0400

    Style input container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const surpriseOptions = [
+    "A blue ostrich eating melon",
+    "A matisse style shark on the telephone",
+    "A pineapple sunbathing on an island",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <section className="search-section">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start with a detailed description
+          <span className="surprise">Surprise me</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="input-container">
+          <input placeholder="An impressionist oil painting of a sunflower in a purple vase..." />
+          <button>Generate</button>
+        </div>
+      </section>
+      <section className="image-section"></section>
     </div>
   );
-}
+};
 
 export default App;
